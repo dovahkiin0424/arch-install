e636f71fb5746da26c5422a2c8749cb4d50cdb52
dovahkiin0424 <dovahkiin0424@gmail.com>
2021-10-24 15:09:14 +0200

    W.I.P

--- arch_install.sh
+++ arch_install.sh
@@ -5,6 +5,7 @@ green=$(tput setaf 2)
 red=$(tput setaf 1)
 white=$(tput sgr0)
 
+clear
 echo "${green}Welcome to my arch install script!${white}"
 
 lsblk
@@ -41,38 +42,30 @@ echo "${green}Base install...${white}"
 
 echo "${green}Installiing the kernel...${white}"
 
-pacstrap /mnt base base-devel linux linux-firmware neovim
+pacstrap /mnt base base-devel linux linux-firmware neovim networkmanager grub efibootmgr
 
 echo "${green}Generating fstab file...${white}"
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "${green}Entering to the installed system as root...${white}"
-
-arch-chroot /mnt /bin/bash
-
-echo "${green}Installing networkmanager and grub...${white}"
-
-pacman --color always --noconfirm -S networkmanager grub efibootmgr
-
 echo "${green}Enabling NetworkManager.service...${white}"
 
-systemctl enable --now NetworkManager
+arch-chroot /mnt systemctl enable --now NetworkManager
 
 read -p "${blue}Which drive do you want to install grub? (sdx) ${white}" drive
 
-grub-install /dev/$drive
+arch-chroot /mnt grub-install /dev/$drive
 
 echo "${green}Configuring grub...${white}"
 
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "${green}Making a new root password...${white}"
 
-passwd
+arch-chroot /mnt passwd
 
 read -p "${blue}Hostname: ${white}" host
 
-echo $host > /etc/hostname
+arch-chroot /mnt echo $host > /etc/hostname
 
 echo "${green}I'm done, have fun with your brand new arch installation!${white}"
